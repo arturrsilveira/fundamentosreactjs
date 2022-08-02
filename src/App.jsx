2fe225c58e7629557f9542981452015c53f6ccd4
Artur Rafael da Silveira <arturrsilveira85@gmail.com>
2022-08-01 22:15:37 -0300

    Estrutura da Aplicação - Componenente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post" ;
+import { Sidebar } from "./components/Sidebar";
+
+
+import styles from './App.module.css';
 
 import './global.css';
 
@@ -8,14 +12,19 @@ export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Artur Rafael da Silveira" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quam a voluptatum architecto error iste blanditiis repudiandae? Molestias omnis provident assumenda error, consequuntur sit temporibus optio magnam corporis sunt eveniet?"
-      />
-      <Post 
-        author="Simone da Silveira" 
-        content="Nova foto"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Artur Rafael da Silveira" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quam a voluptatum architecto error iste blanditiis repudiandae? Molestias omnis provident assumenda error, consequuntur sit temporibus optio magnam corporis sunt eveniet?"
+          />
+          <Post 
+            author="Simone da Silveira" 
+            content="Nova foto"
+          />
+        </main>
+      </div>
     </div>
     
   );
