44992ff456d4e56e46b2c14250063c3ffd9a91c3
Artur Rafael da Silveira <arturrsilveira85@gmail.com>
2022-08-02 22:07:12 -0300

    Commit: Estrutura da aplicação - Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import {PencilLine} from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -8,12 +10,19 @@ export function Sidebar() {
             />
             
             <div className={styles.profile}>
+
+                <img className={styles.avatar} src="https://media-exp1.licdn.com/dms/image/C4D03AQFxprbrrlwugw/profile-displayphoto-shrink_100_100/0/1600274138294?e=1665014400&v=beta&t=WMsejbGvZ6T5fE23qU0q7awaa-ak8A54fSCUiQHohzY" 
+                alt="Imagem de Perfil"/>
+
                 <strong>Artur Rafael da Silveira</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     );
